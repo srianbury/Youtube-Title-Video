78df5169ede41a6e90839316dc6f435bb2d44d93
Brian Sunbury <bsunbury29@gmail.com>
2020-02-22 13:05:05 -0700

    allow json format

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,26 +3,47 @@ import fetch from "node-fetch";
 
 const router = Router();
 
-const DATE = "date";
-const VIEW_COUNT = "viewcount";
+const ORDER_DATE = "date";
+const ORDER_VIEW_COUNT = "viewcount";
+
+const FORMAT_LINK = "link";
+const FORMAT_JSON = "json";
 
 /*
   Returns the url for
     - most recent (date) (default)
     - most viewed (viewcount)
   given a channel ID
+  
+  Options
+    - Order
+      - date (default)
+      - viewcount
+    - Format
+     - link (default)
+     - json
 */
 router.get("/:channelId", async (req, res) => {
   const BASE = "https://www.googleapis.com/youtube/v3/search";
   const GRAPHQUERY_PART = "snippet,id";
+
   const channelId = req.params.channelId;
-  const order = req.query.order || DATE;
-  const maxResults = order === DATE ? 1 : 2;
+  const order = req.query.order || ORDER_DATE;
+  const format = req.query.format || FORMAT_LINK;
+
+  const maxResults = order === ORDER_DATE ? 1 : 2;
   const url = `${BASE}?key=${process.env.YOUTUBE_API_KEY}&channelId=${channelId}&part=${GRAPHQUERY_PART}&order=${order}&maxResults=${maxResults}`;
+
   const response = await fetch(url);
   const result = await response.json();
-
   let videoUrl = getUrl(order, result);
+
+  if (format === FORMAT_JSON) {
+    return res.send({
+      videoId: videoUrl
+    });
+  }
+
   videoUrl = watchFormat(videoUrl);
   return res.redirect(videoUrl);
 });
@@ -30,7 +51,7 @@ router.get("/:channelId", async (req, res) => {
 function getUrl(order, result) {
   let videoUrl;
   switch (order) {
-    case VIEW_COUNT:
+    case ORDER_VIEW_COUNT:
       videoUrl = result.items[1].id.videoId;
       break;
     default:
